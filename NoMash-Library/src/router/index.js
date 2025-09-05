d20244163fa40944ddfae3d6e60628fcc31d4115
Sun <hsun0069@student.monash.edu>
2025-09-05 15:34:55 +0800

    week5

---
+++ NoMash-Library/src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import LoginView from '@/views/LoginView.vue'
+import AccessDenied from '@/views/AccessDenied.vue'
+import isAuthenticated from '@/authenticate'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: AboutView
+    },
+    {
+        path: '/access-denied',
+        name: 'AccessDenied',
+        component: AccessDenied
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: LoginView
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.name === 'AccessDenied') {
+        next()
+    } else if (to.name !== 'Login' && !isAuthenticated.value) next({ name: 'AccessDenied'})
+    else next()
+})
+
+export default router
